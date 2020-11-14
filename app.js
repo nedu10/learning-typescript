6d454a1a999c0dfb80a5cb916a9cff192fadaefd
Chinedu Ifediorah <cifediorah3@gmail.com>
2020-11-14 13:34:14 +0100

    initial commit setting up typescript

---
+++ app.js
@@ -0,0 +1,2 @@
+var person = "My name is chinedu";
+var second_person = "I am also chinedu";
---
+++ app.ts
@@ -0,0 +1,3 @@
+const person: String = "My name is chinedu";
+
+const second_person: String = "I am also chinedu";
